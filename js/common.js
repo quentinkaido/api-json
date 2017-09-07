c34a9e0892921218987abfa94149286fba9e54a6
philippepary <philippe@scil.coop>
2017-09-07 12:10:50 +0200

    Display of people is OK

--- js/common.js
+++ js/common.js
@@ -1,259 +1,46 @@
-var rawData = JSON.parse('' +
-'{' +
-'' +
-'    "items": [' +
-'        {' +
-'            "index": 1,' +
-'            "age": 65,' +
-'            "size": 4.9,' +
-'            "name": "Nicole",' +
-'            "surname": "Bolton",' +
-'            "fullname": "Debra Bender",' +
-'            "email": "bob@marsh.eh",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 2,' +
-'            "age": 65,' +
-'            "size": 2.02,' +
-'            "name": "Shawn",' +
-'            "surname": "Dyer",' +
-'            "fullname": "Leon Barber",' +
-'            "email": "franklin@riley.gr",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 3,' +
-'            "age": 65,' +
-'            "size": 5.47,' +
-'            "name": "Shannon",' +
-'            "surname": "Cain",' +
-'            "fullname": "Allison Padgett",' +
-'            "email": "martin@bradshaw.sk",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 4,' +
-'            "age": 65,' +
-'            "size": 1.42,' +
-'            "name": "Willie",' +
-'            "surname": "Brock",' +
-'            "fullname": "Helen Hewitt",' +
-'            "email": "floyd@woodward.af",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 5,' +
-'            "age": 65,' +
-'            "size": 1.66,' +
-'            "name": "Alan",' +
-'            "surname": "Butler",' +
-'            "fullname": "Dolores McKnight",' +
-'            "email": "douglas@tilley.vg",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 6,' +
-'            "age": 65,' +
-'            "size": 4.97,' +
-'            "name": "Franklin",' +
-'            "surname": "Tyler",' +
-'            "fullname": "William Rowe",' +
-'            "email": "russell@crabtree.sb",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 7,' +
-'            "age": 65,' +
-'            "size": 2.87,' +
-'            "name": "Theresa",' +
-'            "surname": "Christensen",' +
-'            "fullname": "Judy Riley",' +
-'            "email": "joseph@raynor.co",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 8,' +
-'            "age": 65,' +
-'            "size": 2.58,' +
-'            "name": "Lois",' +
-'            "surname": "Vincent",' +
-'            "fullname": "Sheryl Farmer",' +
-'            "email": "herbert@harmon.ch",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 9,' +
-'            "age": 65,' +
-'            "size": 4.48,' +
-'            "name": "Dennis",' +
-'            "surname": "Cross",' +
-'            "fullname": "Sarah Weiss",' +
-'            "email": "janet@puckett.gg",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 10,' +
-'            "age": 65,' +
-'            "size": 5.82,' +
-'            "name": "Danny",' +
-'            "surname": "Norton",' +
-'            "fullname": "Arthur Burke",' +
-'            "email": "rhonda@lang.nf",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 11,' +
-'            "age": 65,' +
-'            "size": 4.58,' +
-'            "name": "Charlene",' +
-'            "surname": "Horne",' +
-'            "fullname": "Jeanette Love",' +
-'            "email": "melinda@jennings.hk",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 12,' +
-'            "age": 65,' +
-'            "size": 1.68,' +
-'            "name": "Megan",' +
-'            "surname": "Hayes",' +
-'            "fullname": "Pat Carver",' +
-'            "email": "erin@reid.sb",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 13,' +
-'            "age": 65,' +
-'            "size": 4.44,' +
-'            "name": "Christina",' +
-'            "surname": "Morris",' +
-'            "fullname": "Ronnie Chang",' +
-'            "email": "marianne@woodward.bj",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 14,' +
-'            "age": 65,' +
-'            "size": 1.77,' +
-'            "name": "Michelle",' +
-'            "surname": "Curry",' +
-'            "fullname": "Andrew Welch",' +
-'            "email": "kyle@brantley.er",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 15,' +
-'            "age": 65,' +
-'            "size": 1.05,' +
-'            "name": "Frances",' +
-'            "surname": "Thornton",' +
-'            "fullname": "Charlotte Walsh",' +
-'            "email": "crystal@bowling.ng",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 16,' +
-'            "age": 65,' +
-'            "size": 2.34,' +
-'            "name": "Bill",' +
-'            "surname": "Simpson",' +
-'            "fullname": "Sarah Heath",' +
-'            "email": "walter@wilkerson.eh",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 17,' +
-'            "age": 65,' +
-'            "size": 3.74,' +
-'            "name": "Kyle",' +
-'            "surname": "Lamb",' +
-'            "fullname": "Dan Conway",' +
-'            "email": "marc@mckenna.cr",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 18,' +
-'            "age": 65,' +
-'            "size": 3.68,' +
-'            "name": "Lois",' +
-'            "surname": "Burnette",' +
-'            "fullname": "Beth Nguyen",' +
-'            "email": "audrey@burnette.na",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 19,' +
-'            "age": 65,' +
-'            "size": 5.53,' +
-'            "name": "Caroline",' +
-'            "surname": "Ballard",' +
-'            "fullname": "Dennis Rose",' +
-'            "email": "nicholas@clapp.al",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 20,' +
-'            "age": 65,' +
-'            "size": 1.38,' +
-'            "name": "Rebecca",' +
-'            "surname": "Carver",' +
-'            "fullname": "Norman McPherson",' +
-'            "email": "florence@fleming.ss",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 21,' +
-'            "age": 65,' +
-'            "size": 5.7,' +
-'            "name": "Ashley",' +
-'            "surname": "Rosenberg",' +
-'            "fullname": "Edna Law",' +
-'            "email": "stacey@chang.org",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 22,' +
-'            "age": 65,' +
-'            "size": 4.86,' +
-'            "name": "Matthew",' +
-'            "surname": "Cline",' +
-'            "fullname": "Jerome Wiley",' +
-'            "email": "audrey@cooke.tr",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 23,' +
-'            "age": 65,' +
-'            "size": 5.22,' +
-'            "name": "Franklin",' +
-'            "surname": "Mueller",' +
-'            "fullname": "Kristine Garrett",' +
-'            "email": "allison@boykin.by",' +
-'            "drivinglicence": true' +
-'        },' +
-'        {' +
-'            "index": 24,' +
-'            "age": 65,' +
-'            "size": 4.25,' +
-'            "name": "Milton",' +
-'            "surname": "Nichols",' +
-'            "fullname": "Catherine Davidson",' +
-'            "email": "lillian@mcnamara.id",' +
-'            "drivinglicence": false' +
-'        },' +
-'        {' +
-'            "index": 25,' +
-'            "age": 65,' +
-'            "size": 1.16,' +
-'            "name": "Melinda",' +
-'            "surname": "Bowles",' +
-'            "fullname": "George Wolf",' +
-'            "email": "christy@grossman.tt",' +
-'            "drivinglicence": true' +
-'        }' +
-'    ]' +
-'' +
-'}' +
-'');
-console.log(rawData.items);
+/* We create an app variable to avoid having to write a long
+document.getElementById… everytime.
+The HTML div with id="application" is the core of our software */
+var app = document.getElementById("application");
+
+// Function to display all the people
+var showPeople = function() {
+  // First, we empty the app's HTML
+  app.innerHTML = "";
+  // Then we display a card for each people
+  people.forEach(function(person,index) {
+    // We prepare the whole HTML code in a variable called personCard
+    personCard = "<div class='col-lg-3 col-md-4 col-xs-6'>"; // Begin of column
+    personCard += "<div class='panel panel-default'>"; // Begin of panel
+    // Panel title: person's name and driving licence
+    personCard += "<div class='panel-heading'><h1 class='panel-title'>" + person.name;
+    if (person.drivinglicence) {
+      personCard += "&nbsp;<span class='label label-success'>permis B</span>";
+    }
+    personCard += "</h1></div>";
+    // Panel body : person's age and contact/edition/suppression buttons
+    personCard += "<div class='panel-body'>";
+    personCard += "<div class='alert alert-info'>" + person.age + "&nbsp;ans</div>";
+    personCard += "<div class='btn-group'>"; // Begin of buttons group
+    // Mailto button : email set in href attribute
+    personCard += "<a class='btn btn-info' href='mailto:" + person.email +"'>Contacter</a>";
+    // Edit button: index (it's the current position in the people table) is given as GET param
+    personCard += "<a class='btn btn-warning' href='edit.html?id=" + index + "'>Éditer</a>";
+    // Delete button: index (it's the current position in the people table) is given as GET param
+    personCard += "<a class='btn btn-danger' href='delete.html?id=" + index + "'>Supprimer</a>";
+    personCard += "</div>" // End of buttons group
+    personCard += "</div>"; // End of panel
+    personCard += "</div>"; // End of column
+    // Card is ready : we add it to the app's HTML
+    app.innerHTML += personCard;
+});
+}
+
+var printInfo = function() {
+  // Display the number of people in the navbar
+  document.getElementById("infoZone").innerHTML = "&nbsp;Il y a " + people.length + " personnes en base";
+  // Display the number of people in the app's div
+  app.innerHTML = "&nbsp;Il y a " + people.length + " personnes en base";
+}
+// Whatever happens, we launch printInfo() everytime
+printInfo();
